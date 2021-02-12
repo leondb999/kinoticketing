1bf2112d22c9ca86475cab14852a3b0f339c19a4
leondb999 <led.1999led@gmail.com>
2021-02-12 09:41:33 +0100

    sql Azure Connection

---
+++ src/Querys.sql
@@ -0,0 +1,31 @@
+/*CREATE TABLE Film (
+    Titel  varchar(255),
+    
+);
+*/
+/*
+INSERT INTO Film (Titel) Values ('Star Wars')
+INSERT INTO Film (Titel) Values ('Herr der Ringe')
+INSERT INTO Film (Titel) Values ('Iron Man')
+*/
+
+
+/*
+Create Table Person (
+    ID int IDENTITY(1,1),
+    Vorname varchar(255),
+    Nachname varchar(255),
+    EMail varchar(255),
+    PLZ int,
+    Straße varchar(255),
+    HausNr int,
+    PW varchar(255)
+)
+*/
+
+
+/*
+INSERT INTO Person (Vorname, Nachname, EMail, PLZ, Straße, HausNr, PW) VALUES ('Leon', 'Dickob', 'leondickob@gmail.com', '55257', 'Uhlerborner Weg', '3', 'HelloWorld')
+INSERT INTO Person (Vorname, Nachname, EMail, PLZ, Straße, HausNr, PW) VALUES ('Nicolas', 'Schneider', 'nicolas.alsfeld@outlook.com', '123213', 'Coblitzallee', '9', '12345')
+*/
+
