05206d73783536acf4538132606535c0b5a30767
leondb999 <led.1999led@gmail.com>
2021-02-12 10:23:56 +0100

    Wokring SQL Querys SEXYYYYYYY

--- src/sqltest/sqltest.js
+++ src/sqltest/sqltest.js
@@ -1,3 +1,4 @@
+
 var Connection = require('tedious').Connection;  
     var config = {  
         server: 'kinoticketsystem-db-server.database.windows.net',  //update me
@@ -13,13 +14,39 @@ var Connection = require('tedious').Connection;
             encrypt: true,
             database: 'Kinoticketsystem-DB'  //update me
         }
-    };  
+    }; 
     var connection = new Connection(config);  
     connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
+        // If no error, then good to proceed.  
         console.log("Connected");  
-    });
+        executeStatement();  
+    });  
     
     connection.connect();
-
-
+  
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+  
+    function executeStatement() {  
+        request = new Request("SELECT * FROM Person;", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = "";  
+        request.on('row', function(columns) {  
+            columns.forEach(function(column) {  
+              if (column.value === null) {  
+                console.log('NULL');  
+              } else {  
+                result+= column.value + " ";  
+              }  
+            });  
+            console.log(result);  
+            result ="";  
+        });  
+  
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+    }
