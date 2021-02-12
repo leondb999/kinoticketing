7cbebcf6692eab421465a6342a739905afd13225
leondb999 <led.1999led@gmail.com>
2021-02-12 10:11:31 +0100

    working sql Azure Connection

--- src/sqltest/sqltest.js
+++ src/sqltest/sqltest.js
@@ -1,65 +1,25 @@
-
-// Azure SQL DB
-const { Connection, Request } = require("tedious");
-
-// Create connection to database
-const config = {
-  authentication: {
-    options: {
-      userName: "kinoadmin", // update me
-      password: "1234KinoLeonNico5678" // update me
-    },
-    type: "default"
-  },
-  server: "kinoticketsystem-db-server.database.windows.net", // update me
-  options: {
-    database: "Kinoticketsystem-DB", //update me
-    encrypt: true
-  }
-};
-
-const connection = new Connection(config);
-
-
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-    Console.log("hello")
-    queryDatabase();
- /*
-    if (err) {
-    console.error(err.message);
-    
-  } else {
-      
-    queryDatabase();
-    console.log("Read from db")
-  }
-*/});
-
-function queryDatabase() {
-  console.log("Reading rows from the Table...");
-
-  // Read all rows from table
-  const request = new Request(
-    `SELECT *     
-     FROM dbo.Person
-    `,
-    (err, rowCount) => {
-      if (err) {
-        console.error(err.message);
-      
-      } else {
-        console.log(`${rowCount} row(s) returned`);
-      }
-    }
-  );
-
-  request.on("row", columns => {
-    columns.forEach(column => {
-      console.log("%s\t%s", column.metadata.colName, column.value);
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'kinoticketsystem-db-server.database.windows.net',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'kinoadmin', //update me
+                password: '1234KinoLeonNico5678'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'Kinoticketsystem-DB'  //update me
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connected");  
     });
-  });
+    
+    connection.connect();
 
-  connection.execSql(request);
-}
-queryDatabase()
+
